33fb7a85ee913841fba9c5ac7072e861c1166be1
Lorena Cueva <lorenacueva@gmail.com>
2022-04-28 12:16:56 -0700

    commit tree d004dc4225a6f52a3de068496a92876b563b2daf

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-\s']+/g, "")
+  }
+  static titleize(str){
+    const avoid = ["a", "an", "but", "of", "for", "at", "by", "from", "and", "the"]
+    let newWord = []
+    str.split(' ').forEach(word => {
+      if(avoid.includes(word))newWord.push(word)
+      else newWord.push(this.capitalize(word))
+    })
+    return this.capitalize(newWord.join(' '))
+  }
 }
+
+console.log(Formatter.capitalize("lore"))
+console.log(Formatter.sanitize("Lo-r#$$$e ^&Cuev#a"))
+console.log(Formatter.titleize("the red and green house by the lake"))
